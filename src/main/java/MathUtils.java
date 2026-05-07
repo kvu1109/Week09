eabe1cbd34c0e5401c953036522f17ef5c9305d6
kvu1109 <vutrankhoi1109@gmail.com>
2026-05-07 07:59:08 +0700

    violate checkstyle

--- src/main/java/MathUtils.java
+++ src/main/java/MathUtils.java
@@ -1,9 +1,6 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Lớp tiện ích cung cấp các phép tính cơ bản.
- */
 public class MathUtils {
   // Thay đổi nhỏ trong code để test caching
   // Logger là static final: một instance duy nhất per class, thread-safe.
